5f4d0c6a77dd8a32aaa99b26919b07dbcd7baaa9
Flavio Ciriaco de Moura <flaviociriaco@yahoo.com>
2023-04-15 21:13:23 -0300

    adicionando funções para ver quem venceu o jogo

--- script.js
+++ script.js
@@ -53,6 +53,8 @@ function renderSquare() {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+
+    checkGame()
 }
 
 function renderInfo() {
@@ -63,4 +65,36 @@ function renderInfo() {
 function toggleplayer() {
     player = (player === 'x') ? 'o' : 'x';
     renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerFor ('x')) {
+        warning = 'O  "x" venceu';
+        playing = false;
+    }else if(checkWinnerFor('o')) {
+        warning =  'O "o" venceu';
+        playing = false;
+    }else if(isFull()) {
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(player) {
+ let pos = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
+
+    'a1,b1,c1',
+    'a2,b2,c3',
+    'a3,b3,c3',
+
+    'a1,b2,c3',
+    'a3,b2,c1'
+ ]
+}
+
+function isFull() {
+
 }
